3695723252460d3b0fc5caf1ddd82c966edd37f1
Bubhux <bubhuxpaindepice@gmail.com>
2023-10-29 14:37:09 +0100

    Création de la fonction load_mock_clubs

--- server.py
+++ server.py
@@ -30,6 +30,28 @@ class NotEnoughPointsException(Exception):
     flash_message = 'You do not have enough points to make this booking.'
 
 
+clubs_load_message = None
+competitions_load_message = None
+
+def load_mock_clubs():
+    return [
+        {
+            "name": "Simply Lift",
+            "email": "john@simplylift.co",
+            "points": "13"
+        },
+        {
+            "name": "Iron Temple",
+            "email": "admin@irontemple.com",
+            "points": "4"
+        },
+        {
+            "name": "She Lifts",
+            "email": "kate@shelifts.co.uk",
+            "points": "12"
+        }
+    ]
+
 def loadClubs():
     global clubs_load_message
     filename = 'clubs.json'
